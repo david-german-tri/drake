9b2e3b9814cb7b4eb687e31081f3802bb57a9e6a
David German <david.german@tri.global>
2016-02-25 13:39:49 -0500

    Incomplete pieces of cleanup, squash this commit before pushing

--- drake/systems/cascade_system.h
+++ drake/systems/cascade_system.h
@@ -1,113 +1,90 @@
-// Copyright 2016 The Drake Authors
-#ifndef DRAKE_SYSTEMS_CASCADE_SYSTEM_H_
-#define DRAKE_SYSTEMS_CASCADE_SYSTEM_H_
-
-#include <memory>
-#include "drake/core/Core.h"
-#include "drake/systems/System.h"
+#ifndef DRAKE_CASCADE_SYSTEM_H
+#define DRAKE_CASCADE_SYSTEM_H
 
 namespace Drake {
 
-/** CascadeSystem<System1,System2>
+/** CascadeSystem<ScalarType, System1,System2>
  * @brief Builds a new system from the cascade connection of two simpler systems
  * @concept{system_concept}
  *
- * ![Cascade combination of two
- * systems](http://underactuated.csail.mit.edu/figures/cascade_system.svg)
+ * ![Cascade combination of two systems](http://underactuated.csail.mit.edu/figures/cascade_system.svg)
  *
  */
-template <class System1, class System2>
-class CascadeSystem {
- public:
-  template <typename ScalarType>
-  using StateVector = typename CombinedVectorUtil<
-      System1::template StateVector,
-      System2::template StateVector>::template type<ScalarType>;
-  template <typename ScalarType>
-  using StateVector1 = typename System1::template StateVector<ScalarType>;
-  template <typename ScalarType>
-  using StateVector2 = typename System2::template StateVector<ScalarType>;
-  template <typename ScalarType>
-  using InputVector = typename System1::template InputVector<ScalarType>;
-  template <typename ScalarType>
-  using System1OutputVector =
-      typename System1::template OutputVector<ScalarType>;
-  template <typename ScalarType>
-  using OutputVector = typename System2::template OutputVector<ScalarType>;
-  typedef std::shared_ptr<System1> System1Ptr;
-  typedef std::shared_ptr<System2> System2Ptr;
-  typedef CombinedVectorUtil<StateVector1, StateVector2> util;
+template <class ScalarType, class System1, class System2>
+class CascadeSystem
+    : public BaseSystem<ScalarType, std::pair<typename System1::StateVector, typename System2::StateVector>,
+                             typename System1::InputVector, typename System2::OutputVector>
+{
+public:
+  static_assert(std::is_same<typename System1::OutputVector, typename System2::InputVector>::value,
+                "System 2 input vector must match System 1 output vector");
 
-  static_assert(
-      std::is_same<typename System1::template OutputVector<double>,
-                   typename System2::template InputVector<double>>::value,
-      "System 2 input vector must match System 1 output vector");
+  using StateVector = typename std::pair<typename System1::StateVector, typename System2::StateVector>;
+  using InputVector = typename System1::InputVector;
+  using OutputVector = typename System2::OutputVector;
 
-  CascadeSystem(const System1Ptr& _sys1, const System2Ptr& _sys2)
-      : sys1(_sys1), sys2(_sys2) {}
+  CascadeSystem(std::shared_ptr<System1> sys1, std::shared_ptr<System2> sys2) : sys1_(sys1), sys2_(sys2){};
 
-  template <typename ScalarType>
-  StateVector<ScalarType> dynamics(const ScalarType& t,
-                                   const StateVector<ScalarType>& x,
-                                   const InputVector<ScalarType>& u) const {
-    auto x1 = util::first(x);
-    auto x2 = util::second(x);
-    System1OutputVector<ScalarType> y1 = sys1->output(t, x1, u);
-    StateVector<ScalarType> xdot =
-        util::combine(sys1->dynamics(t, x1, u), sys2->dynamics(t, x2, y1));
+  StateVector dynamics(const ScalarType& t, const StateVector& x, const InputVector& u) override
+  {
+    const auto& x1 = x.first;
+    const auto& x2 = x.second;
+    typename System1::OutputVector y1 = sys1_->output(t, x1, u);
+    StateVector xdot = std::make_pair(sys1_->dynamics(t, x1, u), sys2_->dynamics(t, x2, y1));
     return xdot;
   }
 
-  template <typename ScalarType>
-  OutputVector<ScalarType> output(const ScalarType& t,
-                                  const StateVector<ScalarType>& x,
-                                  const InputVector<ScalarType>& u) const {
-    auto x1 = util::first(x);
-    auto x2 = util::second(x);
-    System1OutputVector<ScalarType> y1 = sys1->output(t, x1, u);
-    OutputVector<ScalarType> y2 = sys2->output(t, x2, y1);
+  OutputVector output(const ScalarType& t, const StateVector& x, const InputVector& u) override
+  {
+    const auto& x1 = x.first;
+    const auto& x2 = x.second;
+    typename System1::OutputVector y1 = sys1_->output(t, x1, u);
+    OutputVector y2 = sys2_->output(t, x2, y1);
     return y2;
   }
 
-  bool isTimeVarying() const {
-    return sys1->isTimeVarying() || sys2->isTimeVarying();
+  bool isTimeVarying() const override
+  {
+    return sys1_->isTimeVarying() || sys2_->isTimeVarying();
   }
-  bool isDirectFeedthrough() const {
-    return sys1->isDirectFeedthrough() && sys2->isDirectFeedthrough();
+  bool isDirectFeedthrough() const override
+  {
+    return sys1_->isDirectFeedthrough() && sys2_->isDirectFeedthrough();
   }
-  size_t getNumStates() const {
-    return Drake::getNumStates(*sys1) + Drake::getNumStates(*sys2);
+  size_t getNumStates() const override
+  {
+    return Drake::getNumStates(*sys1_) + Drake::getNumStates(*sys2_);
   }
-  size_t getNumInputs() const { return Drake::getNumInputs(*sys1); }
-  size_t getNumOutputs() const { return Drake::getNumOutputs(*sys2); }
-
- public:
-  const System1Ptr& getSys1() const { return sys1; }
-
-  const System2Ptr& getSys2() const { return sys2; }
-
-  friend StateVector<double> getInitialState(
-      const CascadeSystem<System1, System2>& sys) {
-    return util::combine(getInitialState(*(sys.sys1)),
-                         getInitialState(*(sys.sys2)));
+  size_t getNumInputs() const override
+  {
+    return Drake::getNumInputs(*sys1_);
+  }
+  size_t getNumOutputs() const override
+  {
+    return Drake::getNumOutputs(*sys2_);
   }
 
- private:
-  System1Ptr sys1;
-  System2Ptr sys2;
+  static StateVector getInitialState(const CascadeSystem<ScalarType, System1, System2>& sys)
+  {
+    return std::make_pair(getInitialState(*(sys.sys1_)), getInitialState(*(sys.sys2_)));
+  }
+
+private:
+  std::shared_ptr<System1> sys1_;
+  std::shared_ptr<System2> sys2_;
 };
 
-/** cascade(sys1, sys2)
+/** cascade(sys1, sys2_)
  * @brief Convenience method to create a cascade combination of two systems
  * @ingroup modeling
  */
-template <typename System1, typename System2>
-std::shared_ptr<CascadeSystem<System1, System2>> cascade(
-    const std::shared_ptr<System1>& sys1,
-    const std::shared_ptr<System2>& sys2) {
-  return std::make_shared<CascadeSystem<System1, System2>>(sys1, sys2);
-}
+template <typename ScalarType, typename System1, typename System2>
+std::shared_ptr<CascadeSystem<ScalarType, System1, System2>> cascade(std::shared_ptr<System1> sys1,
+                                                                     std::shared_ptr<System2> sys2)
+{
+  return std::make_shared<CascadeSystem<ScalarType, System1, System2>>(sys1, sys2);
+};
 
 }  // namespace Drake
 
-#endif  // DRAKE_SYSTEMS_CASCADE_SYSTEM_H_
+#endif  // DRAKE_CASCADE_SYSTEM_H
--- drake/systems/feedback_system.h
+++ drake/systems/feedback_system.h
@@ -1,10 +1,5 @@
-// Copyright 2016 The Drake Authors
 #ifndef DRAKE_SYSTEMS_FEEDBACK_SYSTEM_H_
 #define DRAKE_SYSTEMS_FEEDBACK_SYSTEM_H_
-
-#include <memory>
-#include "drake/core/Core.h"
-#include "drake/systems/System.h"
 
 namespace Drake {
 
@@ -18,128 +13,116 @@ namespace Drake {
  *
  */
 
-template <class System1, class System2>
-class FeedbackSystem {
- public:
-  template <typename ScalarType>
-  using StateVector1 = typename System1::template StateVector<ScalarType>;
-  template <typename ScalarType>
-  using StateVector2 = typename System2::template StateVector<ScalarType>;
-  template <typename ScalarType>
-  using StateVector = typename CombinedVectorUtil<
-      System1::template StateVector,
-      System2::template StateVector>::template type<ScalarType>;
-  template <typename ScalarType>
-  using InputVector = typename System1::template InputVector<ScalarType>;
-  template <typename ScalarType>
-  using OutputVector = typename System1::template OutputVector<ScalarType>;
-  typedef CombinedVectorUtil<StateVector1, StateVector2> util;
+template <class ScalarType, class System1, class System2>
+class FeedbackSystem
+    : public BaseSystem<ScalarType, std::pair<typename System1::StateVector, typename System2::StateVector>,
+                             typename System1::InputVector, typename System1::OutputVector>
+{
+public:
+  static_assert(std::is_same<typename System1::OutputVector, typename System2::InputVector>::value,
+                "System 2 input vector must match System 1 output vector");
+  static_assert(std::is_same<typename System2::OutputVector, typename System1::InputVector>::value,
+                "System 1 input vector must match System 2 output vector");
 
-  typedef std::shared_ptr<System1> System1Ptr;
-  typedef std::shared_ptr<System2> System2Ptr;
+  using StateVector = typename std::pair<typename System1::StateVector, typename System2::StateVector>;
+  using InputVector = typename System1::InputVector;
+  using OutputVector = typename System1::OutputVector;
 
-  static_assert(
-      std::is_same<typename System1::template OutputVector<double>,
-                   typename System2::template InputVector<double>>::value,
-      "System 2 input vector must match System 1 output vector");
-  static_assert(
-      std::is_same<typename System2::template OutputVector<double>,
-                   typename System1::template InputVector<double>>::value,
-      "System 1 input vector must match System 2 output vector");
+  FeedbackSystem(std::shared_ptr<System1> sys1, std::shared_ptr<System2> sys2_) : sys1_(sys1_), sys2_(sys2_){};
 
-  FeedbackSystem(const System1Ptr& _sys1, const System2Ptr& _sys2)
-      : sys1(_sys1), sys2(_sys2) {}
+  StateVector dynamics(const ScalarType& t, const StateVector& x, const InputVector& u) override
+  {
+    OutputVector y1;
+    InputVector y2;
+    const auto& x1 = x.first;
+    const auto& x2 = x.second;
+    subsystemOutputs(t, x1, x2, u, y1, y2);
 
-  template <typename ScalarType>
-  StateVector<ScalarType> dynamics(const ScalarType& t,
-                                   const StateVector<ScalarType>& x,
-                                   const InputVector<ScalarType>& u) const {
-    OutputVector<ScalarType> y1;
-    InputVector<ScalarType> y2;
-    auto x1 = util::first(x);
-    auto x2 = util::second(x);
-    subsystemOutputs(t, x1, x2, u, &y1, &y2);
-
-    StateVector<ScalarType> xdot = util::combine(
-        sys1->dynamics(t, x1, static_cast<InputVector<ScalarType>>(toEigen(y2) +
-                                                                   toEigen(u))),
-        sys2->dynamics(t, x2, y1));
+    StateVector xdot = std::make_pair(sys1_->dynamics(t, x1, static_cast<InputVector>(toEigen(y2) + toEigen(u))),
+                                      sys2_->dynamics(t, x2, y1));
     return xdot;
   }
 
-  template <typename ScalarType>
-  OutputVector<ScalarType> output(const ScalarType& t,
-                                  const StateVector<ScalarType>& x,
-                                  const InputVector<ScalarType>& u) const {
-    OutputVector<ScalarType> y1;
-    auto x1 = util::first(x);
-    if (!sys1->isDirectFeedthrough()) {
-      y1 = sys1->output(t, x1,
-                        u);  // then don't need u+y2 here, u will be ignored
-    } else {
-      InputVector<ScalarType> y2;
-      auto x2 = util::second(x);
-      y2 = sys2->output(
-          t, x2, y1);  // y1 might be uninitialized junk, but has to be ok
-      y1 = sys1->output(t, x1, static_cast<InputVector<ScalarType>>(
-                                   toEigen(y2) + toEigen(u)));
+  OutputVector output(const ScalarType& t, const StateVector& x, const InputVector& u) override
+  {
+    OutputVector y1;
+    const auto& x1 = x.first;
+    if (!sys1_->isDirectFeedthrough())
+    {
+      y1 = sys1_->output(t, x1, u);  // then don't need u+y2 here, u will be ignored
+    }
+    else
+    {
+      InputVector y2;
+      const auto& x2 = x.second;
+      y2 = sys2_->output(t, x2, y1);  // y1 might be uninitialized junk, but has to be ok
+      y1 = sys1_->output(t, x1, static_cast<InputVector>(toEigen(y2) + toEigen(u)));
     }
     return y1;
   }
 
-  bool isTimeVarying() const {
-    return sys1->isTimeVarying() || sys2->isTimeVarying();
-  }
-  bool isDirectFeedthrough() const { return sys1->isDirectFeedthrough(); }
-  size_t getNumStates() const {
-    return Drake::getNumStates(*sys1) + Drake::getNumStates(*sys2);
-  }
-  size_t getNumInputs() const { return Drake::getNumInputs(*sys1); }
-  size_t getNumOutputs() const { return Drake::getNumOutputs(*sys1); }
-
-  const System1Ptr& getSys1() const { return sys1; }
-
-  const System2Ptr& getSys2() const { return sys2; }
-
-  friend StateVector<double> getInitialState(
-      const FeedbackSystem<System1, System2>& sys) {
-    return util::combine(getInitialState(*(sys.sys1)),
-                         getInitialState(*(sys.sys2)));
+  StateVector CreateStateVector() const override
+  {
+    return std::make_pair(sys1_->CreateStateVector(), sys2_->CreateStateVector());
   }
 
- private:
-  template <typename ScalarType>
-  void subsystemOutputs(const ScalarType& t, const StateVector1<ScalarType>& x1,
-                        const StateVector2<ScalarType>& x2,
-                        const InputVector<ScalarType>& u,
-                        OutputVector<ScalarType>* y1,
-                        InputVector<ScalarType>* y2) const {
-    if (!sys1->isDirectFeedthrough()) {
-      *y1 = sys1->output(t, x1, u);  // output does not depend on u (so it's ok
-                                    // that we're not passing u+y2)
-      *y2 = sys2->output(t, x2, *y1);
-    } else {
-      *y2 = sys2->output(
-          t, x2, *y1);  // y1 might be uninitialized junk, but has to be ok
-      *y1 = sys1->output(t, x1, static_cast<InputVector<ScalarType>>(
-                                   toEigen(*y2) + toEigen(u)));
+  bool isTimeVarying() const override
+  {
+    return sys1_->isTimeVarying() || sys2_->isTimeVarying();
+  }
+  bool isDirectFeedthrough() const override
+  {
+    return sys1_->isDirectFeedthrough();
+  }
+  size_t getNumStates() const override
+  {
+    return Drake::getNumStates(*sys1_) + Drake::getNumStates(*sys2_);
+  }
+  size_t getNumInputs() const override
+  {
+    return Drake::getNumInputs(*sys1_);
+  }
+  size_t getNumOutputs() const override
+  {
+    return Drake::getNumOutputs(*sys1_);
+  }
+
+  static StateVector getInitialState(const FeedbackSystem<ScalarType, System1, System2>& sys)
+  {
+    return std::make_pair(getInitialState(*(sys.sys1_)), getInitialState(*(sys.sys2_)));
+  }
+
+private:
+ void subsystemOutputs(const ScalarType& t, const typename System1::StateVector& x1, const typename System2::StateVector& x2,
+                               const InputVector& u, OutputVector* y1, InputVector* y2)
+  {
+    if (!sys1_->isDirectFeedthrough())
+    {
+      *y1 = sys1_->output(t, x1, u);  // output does not depend on u (so it's ok
+                                     // that we're not passing u+y2)
+      *y2 = sys2_->output(t, x2, *y1);
+    }
+    else
+    {
+      *y2 = sys2_->output(t, x2, *y1);  // y1 might be uninitialized junk, but has to be ok
+      *y1 = sys1_->output(t, x1, static_cast<InputVector>(toEigen(*y2) + toEigen(u)));
     }
   }
 
-  System1Ptr sys1;
-  System2Ptr sys2;
+  std::shared_ptr<System1> sys1_;
+  std::shared_ptr<System2> sys2_;
 };
 
-/** feedback(sys1, sys2)
+/** feedback(sys1, sys2_)
  * @brief Convenience method to create a feedback combination of two systems
  * @ingroup modeling
  */
-template <typename System1, typename System2>
-std::shared_ptr<FeedbackSystem<System1, System2>> feedback(
-    const std::shared_ptr<System1>& sys1,
-    const std::shared_ptr<System2>& sys2) {
-  return std::make_shared<FeedbackSystem<System1, System2>>(sys1, sys2);
-}
+template <typename ScalarType, typename System1, typename System2>
+std::shared_ptr<FeedbackSystem<ScalarType, System1, System2>> feedback(const std::shared_ptr<System1>& sys1,
+                                                                       const std::shared_ptr<System2>& sys2_)
+{
+  return std::make_shared<FeedbackSystem<ScalarType, System1, System2>>(sys1, sys2_);
+};
 
 }  // namespace Drake
 
